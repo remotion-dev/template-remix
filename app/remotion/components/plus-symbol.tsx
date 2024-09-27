4e0c9d0d12911f69b42f1b02ac57de01d353da1c
Jonny Burger <jonathanburger11@gmail.com>
2024-09-27 18:24:04 +0200

    Update template

--- app/remotion/components/plus-symbol.tsx
+++ app/remotion/components/plus-symbol.tsx
@@ -1,26 +1,26 @@
-import { AbsoluteFill } from 'remotion';
+import { AbsoluteFill } from "remotion";
 
 interface Props {
-	verticalOffset?: number;
+  verticalOffset?: number;
 }
 
 export const PlusSymbol = ({ verticalOffset = 0 }: Props) => {
-	return (
-		<AbsoluteFill
-			style={{
-				display: 'flex',
-				alignItems: 'center',
-				top: `calc(42% + ${verticalOffset}px)`,
-			}}
-		>
-			<span
-				style={{
-					fontSize: 130,
-					fontFamily: 'sans-serif',
-				}}
-			>
-				+
-			</span>
-		</AbsoluteFill>
-	);
+  return (
+    <AbsoluteFill
+      style={{
+        display: "flex",
+        alignItems: "center",
+        top: `calc(42% + ${verticalOffset}px)`,
+      }}
+    >
+      <span
+        style={{
+          fontSize: 130,
+          fontFamily: "sans-serif",
+        }}
+      >
+        +
+      </span>
+    </AbsoluteFill>
+  );
 };
